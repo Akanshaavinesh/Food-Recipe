971d8a9266751c98f34b0edcf897331306ab1486
Avinesh <176383@NTTDATA.COM>
2020-06-13 23:26:08 +0530

    first-commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react"
+import ReactDOM from "react-dom";
+//import App from "./test";
+//import MyPractice from "./Practice.js"
+//import Github from "./Github.js"
+import Appmain from "./Appmain.js"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<Appmain />,document.getElementById("root"));
